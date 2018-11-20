39324dba4280d70c855fbcbf28798147bf3c2d0a
Hirotaka <yokokawa.491992@gmail.com>
2018-11-20 14:06:08 +0900

    とりあえず出したい数値は出せた｡ここから少数第2位までの出し方､及びリファクタリング｡

---
+++ BMIアプリ.py
@@ -0,0 +1,24 @@
+# # 例
+# $ python bmi.py
+# Height(m)? > 170
+# Weight(kg)? > 60
+# Your BMI is 20.76
+
+# 計算式
+# 　BMI＝ 体重kg ÷ (身長m)2
+# 　適正体重＝ (身長m)2 ×22
+
+input_Height = int(input("Height(cm) ? > "))
+input_Weight = int(input("Weight(kg) ? > "))
+
+formula = pow(input_Height,2)
+formula_2 = (input_Weight / formula)
+BMI = (formula_2 * 10000)
+
+print(f"Your BMI is {BMI}")
+
+
+
+
+
+
